10784b39da7e7042d48436babe6724a64d6d4b93
KHELIFA Yassine <yacinemosta910@gmail.com>
2026-08-21 18:37:06 +0200

    fixed a bug

--- app/Http/Controllers/Security/AuthController.php
+++ app/Http/Controllers/Security/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Security;
 use App\Enums\RoleEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Security\LoginRequest;
+use App\Models\Student;
+use App\Models\Teacher;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,6 +15,59 @@ class AuthController extends Controller
 
     public function login()
     {
+
+        $students = Student::query()
+            ->with(['user'])
+            ->whereHas(
+                'enrollments',
+                fn($q) => $q->where('class_id', 1)
+                    ->where('academic_year_id', 27)
+                    ->where('status', 'active')
+            )
+            ->withCount([
+                'grades as graded_exam_count' =>
+                fn($g) =>$g->whereNotNull('score')
+                    ->whereHas(
+                        'exam.teachingAssignment',
+                        fn($g) =>  $g->where('class_id', 1)
+                            ->where('academic_year_id', 27)
+                    )
+            ])
+            ->withAvg([
+                'grades' =>
+                fn($g) => $g->whereNotNull('score')
+                    ->whereHas(
+                        'exam.teachingAssignment',
+                        fn($g) =>  $g->where('class_id', 1)
+                            ->where('academic_year_id', 27)
+                    )
+            ], 'score')
+            ->get();
+
+        $report = $students->map(function ($student) {
+
+            $avg_val =  round( (float) $student->grades_avg_score, 2);
+            $performance = 'needs_improvement';
+
+            if ($avg_val >= 12 && $avg_val < 16)
+                $performance = 'good';
+            else if ($avg_val >= 16)
+                $performance = 'excellent';
+
+            return [
+                'student_id' => $student->user->id,
+                'student_name' => $student->full_name,
+                'graded_exam_count' => $student->graded_exam_count,
+                'average_score' => $avg_val,
+                'performance' => $performance,
+            ];
+        })
+            ->sortByDesc('average_score')
+            ->values();
+
+     //   dd($report);
+
+
         return view('auth.login');
     }
 
