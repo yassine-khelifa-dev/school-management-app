5fdcf75f0d1a063d91828bf412a4164668f180c9
KHELIFA Yassine <yacinemosta910@gmail.com>
2026-08-21 21:17:14 +0200

    feat(api): add student creation endpoint with validation

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\StudentStoreRequest;
 use App\Http\Resources\StudentResource;
 use App\Models\Student;
-use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Http\Response;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class StudentController extends Controller
 {
@@ -21,5 +25,30 @@ class StudentController extends Controller
     {
         $student->load('user');
         return new StudentResource($student);
+    }
+
+    public function store(StudentStoreRequest $request)
+    {
+        $data = $request->validated();
+        $newStudent  = DB::transaction(function () use ($data) {
+
+            $newUser = User::create([
+                'email' => $data['email'],
+                'password' =>  Hash::make('password')
+            ]);
+
+            $data['user_id'] = $newUser->id;
+
+            return Student::create(
+                Arr::except($data, ['email'])
+            );
+        });
+
+        $newStudent->load('user');
+
+        return  Response()->json(
+            new StudentResource($newStudent),
+            201
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::name('api.')->group(function () {
     Route::get('students', [StudentController::class, 'index'])->name('students.index');
     Route::get('students/{student}', [StudentController::class, 'show'])->name('students.show');
+    Route::post('students', [StudentController::class, 'store'])->name('students.store');
 });
