253929a1e78b8a1ae09d83351963207f54ed9308
KHELIFA Yassine <yacinemosta910@gmail.com>
2026-08-22 18:00:33 +0200

    feat(api): add role-based authorization for student routes

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,15 @@ Route::post('login', [AuthController::class, 'login'])->name('auth.login');
 
 
 Route::middleware('auth:sanctum')->name('api.')->group(function () {
-    Route::get('students', [StudentController::class, 'index'])->name('students.index');
+
+    Route::middleware('role:admin')->group(function () {
+
+        Route::get('students', [StudentController::class, 'index'])->name('students.index');
+        Route::post('students', [StudentController::class, 'store'])->name('students.store');
+        Route::patch('students/{student}', [StudentController::class, 'update'])->name('students.update');
+        Route::delete('students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
+    });
+
     Route::get('students/{student}', [StudentController::class, 'show'])->name('students.show');
-    Route::post('students', [StudentController::class, 'store'])->name('students.store');
-    Route::patch('students/{student}', [StudentController::class, 'update'])->name('students.update');
-    Route::delete('students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
-
-
     Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
